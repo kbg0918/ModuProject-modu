c594fb4dd9df3215687a23c73e4a7528edffcab7
kimkg <kimbk0109@naver.com>
2022-06-03 10:05:34 +0900

    frontend(Main page, Login page) 상단 메뉴 바 고정, 채팅기능

--- src/main/java/kbg/modu/moduproject/config/WebSocketConfig.java
+++ src/main/java/kbg/modu/moduproject/config/WebSocketConfig.java
@@ -1,20 +1,19 @@
 package kbg.modu.moduproject.config;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 
-@RequiredArgsConstructor
-@Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-    private final WebSocketHandler webSocketHandler;
-
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+@Component
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
+
 }
--- src/main/java/kbg/modu/moduproject/controller/ChattController.java
+++ src/main/java/kbg/modu/moduproject/controller/ChattController.java
@@ -5,14 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-public class ChatController {
-
+public class ChattController {
     @RequestMapping("/mychatt")
     public ModelAndView chatt() {
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("index");
+        mv.setViewName("chatting");
         return mv;
     }
-
-
 }
