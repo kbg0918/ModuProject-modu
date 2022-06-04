2a1621095779b8015c5312e8de5cdf5e2e2c5e53
Kim Byeng Gwan <kimbk0109@naver.com>
2022-06-04 11:09:47 +0900

    채팅기능 구현, frontend 수정 커밋

--- src/main/java/kbg/modu/moduproject/controller/ChatController.java
+++ src/main/java/kbg/modu/moduproject/controller/ChatController.java
@@ -7,10 +7,19 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ChatController {
+
+
+    @GetMapping("/MODUChatting")
+    public String indexFrom(){
+        return "/MODUChat";
+    }
+
+
     @RequestMapping("/modu")
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
